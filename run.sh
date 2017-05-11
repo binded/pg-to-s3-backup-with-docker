6ee46cc83128a4d3a2f0cce06178bcb07dc69f21
Oli Lalonde <olalonde@gmail.com>
2017-05-10 23:00:57 -0700

    use pd_dump

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 set -x
 set -eo pipefail
 
+databases=($DATABASES)
+
 backup_type="postgres"
 s3_bucket_name=${S3_BUCKET_NAME:-"binded-backups"}
 
@@ -12,7 +14,9 @@ datestr=$(date +%Y-%m-%d)
 # PGUSER
 
 # TODO: streaming copy
+for db in "${databases[@]}"; do
+  pg_dump -d "$db" >> /tmp/db.out
+done
 
-pg_dumpall > /tmp/db.out
 
 aws s3 cp /tmp/db.out "s3://${s3_bucket_name}/${backup_type}/pg-backup-${datestr}.dump"
